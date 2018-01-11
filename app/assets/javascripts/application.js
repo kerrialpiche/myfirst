700bf408b96e2f87114d1fdf56eebfb3d6ae96b2
Kerri Alpiche <kerrialpiche@live.com>
2018-01-11 02:59:20 +0000

    added bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -13,4 +14,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
